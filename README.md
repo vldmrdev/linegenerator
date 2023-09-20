93258011a239f07bf5ffe1b10ad8d5d9bc826d83
Python developer <email@shortmyurl.test>
2023-09-20 17:10:22 +0500

    add click, add cli options, add requirements, rename template

--- README.md
+++ README.md
@@ -2,10 +2,25 @@
 
 This is a simple sample-webserver-logs-generator for testing your scripts and algorithms.
 
-### Hot to use:
+### Hot to use examples:
 
-### Examples:
+#### Get help:
 
-### TODO:
-- cli
-- 
+`python3 log-generator.py --help`
+
+```
+Usage: log-generator.py [OPTIONS]
+
+Options:
+  --lines INTEGER        Lines count
+  --output TEXT          Output file
+  --templates_path TEXT  Templates directory path
+  --template TEXT        Template file
+  --help                 Show this message and exit.
+
+```
+
+#### Use example:
+
+`python3 log-generator.py --output test.txt --template web-server-template.txt --lines 10`
+ 
--- log-generator.py
+++ log-generator.py
@@ -1,22 +1,33 @@
 from jinja2 import Environment, FileSystemLoader
-import faker
+from faker import Faker
+import click
 
-fake = faker.Faker('en_US')
-
-environment = Environment(loader=FileSystemLoader("templates/"))
-template = environment.get_template("nginx-log-template.txt")
+fake = Faker()
 
 
-def fake_log():
+def fake_log(templates_path: str = 'templates/', template: str = 'nginx-log-template.txt'):
+    environment = Environment(loader=FileSystemLoader(templates_path))
+    template = environment.get_template(template)
     content = template.render(
         uri=fake.domain_name(),
         ip_v4=fake.ipv4(),
         port='80',
-        http_code='200'
+        http_code='200',
+        method=fake.http_method()
     )
     return content
 
 
-with open('output-log.txt', 'x') as f:
-    for _ in range(3):
-        f.writelines((fake_log()))
+@click.command()
+@click.option('--lines', default=1, help='Lines count')
+@click.option('--output', default='output.txt', help='Output file')
+@click.option('--templates_path', default='templates/', help='Templates directory path')
+@click.option('--template', default='web-server-template.txt', help='Template file name')
+def main(lines: int = 1, output: str = 'output.txt', templates_path='templates/', template='nginx-log-template.txt'):
+    with open(output, '+w') as f:
+        for _ in range(lines):
+            f.writelines((fake_log(templates_path, template)))
+
+
+if __name__ == '__main__':
+    main()
